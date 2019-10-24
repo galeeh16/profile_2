c15c10d3cf991b0d6ae35e3d88b7a76f1415607b
Galih Anggoro Jati <galih@inarts.co.id>
2019-10-25 00:38:28 +0700

    change css and js

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	$('.burger').on('click', function() {
+		$('.nav-link').toggleClass('nav-active');
+		$('.burger').toggleClass('toggle');
+
+		if($('.nav-link').hasClass('nav-active')) {
+			$('body').css('overflow', 'hidden');
+		} else {
+			$('body').css('overflow', 'auto');
+		}
+	});
+
+	$('#home').on('click', function() {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$('.nav-link').toggleClass('nav-active');
+			$('.burger').toggleClass('toggle');
+			$('body').css('overflow', 'auto');
+		}
+
+		$('html, body').animate({
+        	scrollTop: $("body").offset().top
+    	}, 1500);
+	});
+
+	$('#work').on('click', function() {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$('.nav-link').toggleClass('nav-active');
+			$('.burger').toggleClass('toggle');
+			$('body').css('overflow', 'auto');
+		}
+
+		$('html, body').animate({
+        	scrollTop: ($("#work-experiences").offset().top - 80)
+    	}, 500);
+	});
+
+	$('#skill').on('click', function() {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$('.nav-link').toggleClass('nav-active');
+			$('.burger').toggleClass('toggle');
+			$('body').css('overflow', 'auto');
+		}
+		
+		$('html, body').animate({
+        	scrollTop: ($("#skill-section").offset().top - 80)
+    	}, 500);
+	})
+
+});
