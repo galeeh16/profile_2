3677b81a0c1392d03d40b378dcc8dedf81bbb4f0
Galih Anggoro Jati <galih@inarts.co.id>
2019-10-29 16:08:49 +0700

    add section portfolio and image

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,11 +27,15 @@ $(document).ready(function() {
 			$('.nav-link').toggleClass('nav-active');
 			$('.burger').toggleClass('toggle');
 			$('body').css('overflow', 'auto');
-		}
 
-		$('html, body').animate({
-        	scrollTop: ($("#work-experiences").offset().top - 80)
-    	}, 500);
+			$('html, body').animate({
+        		scrollTop: ($("#work-experiences").offset().top - 80)
+    		}, 500);
+		} else {
+			$('html, body').animate({
+	        	scrollTop: ($("#work-experiences").offset().top - 40)
+	    	}, 500);
+		}		
 	});
 
 	$('#skill').on('click', function() {
@@ -39,11 +43,31 @@ $(document).ready(function() {
 			$('.nav-link').toggleClass('nav-active');
 			$('.burger').toggleClass('toggle');
 			$('body').css('overflow', 'auto');
+
+			$('html, body').animate({
+        		scrollTop: ($("#skill-section").offset().top - 80)
+    		}, 500);
+		} else {
+			$('html, body').animate({
+        		scrollTop: ($("#skill-section").offset().top - 40)
+    		}, 500);
 		}
-		
-		$('html, body').animate({
-        	scrollTop: ($("#skill-section").offset().top - 80)
-    	}, 500);
-	})
+	});
+
+	$('#portfolio').on('click', function() {
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			$('.nav-link').toggleClass('nav-active');
+			$('.burger').toggleClass('toggle');
+			$('body').css('overflow', 'auto');
+
+			$('html, body').animate({
+        		scrollTop: ($("#portfolio-section").offset().top - 80)
+    		}, 500);
+		} else {
+			$('html, body').animate({
+        		scrollTop: ($("#portfolio-section").offset().top - 40)
+    		}, 500);
+		}
+	});
 
 });
